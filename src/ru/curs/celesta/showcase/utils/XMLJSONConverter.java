df477123d5ec994d692c19c1dd22bcc6928f08a9
AleXXL <alexxx_was@mail.ru>
2014-04-08 15:34:06 +0000

    Дополнительные настройки для корректной работы json xml конвертера

--- src/ru/curs/celesta/showcase/utils/XMLJSONConverter.java
+++ src/ru/curs/celesta/showcase/utils/XMLJSONConverter.java
@@ -8,7 +8,7 @@ import javax.xml.transform.TransformerException;
 import org.json.JSONException;
 import org.xml.sax.SAXException;
 
-import ru.curs.showcase.util.JSONToXMLParser;
+import ru.curs.celesta.showcase.utils.JSONToXMLParser;
 
 import com.google.gson.JsonElement;
 
