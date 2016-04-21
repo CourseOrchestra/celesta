11ff4cc25ad04f2e97c845ba6427fe7ee5dc59d9
Ivan <iponomarev@mail.ru>
2016-04-21 14:22:38 +0000

    Исправление DBSchemaSync

--- src/ru/curs/celesta/dbschemasync/DBSchemaSync.java
+++ src/ru/curs/celesta/dbschemasync/DBSchemaSync.java
@@ -1,7 +1,9 @@
 package ru.curs.celesta.dbschemasync;
 
 import java.io.File;
+import java.util.Properties;
 
+import ru.curs.celesta.AppSettings;
 import ru.curs.celesta.score.Score;
 
 /**
@@ -25,6 +27,16 @@ public final class DBSchemaSync {
 	public static void main(String[] args) throws Exception {
 		// "c:/Users/Ivan/workspace/celesta/score/"
 		// "c:/Users/Ivan/Desktop/test2.dbs"
+		//[TODO: remove this stub
+		Properties settings = new Properties();
+		settings.setProperty("score.path", ".");
+		settings.setProperty("pylib.path", ".");
+		settings.setProperty("rdbms.connection.url", "jdbc:sqlserver://");
+		//this is why all this stuff is here
+		settings.setProperty("allow.indexed.nulls", "true");
+		AppSettings.init(settings);
+		//TODO]
+		
 		if (args.length < 2)
 			throw new Exception(
 					"There should be two arguments: score path and .dbs-file.");
