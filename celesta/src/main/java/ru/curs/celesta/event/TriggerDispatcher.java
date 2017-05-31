8f867df1d1a9d88cf971c9b35d9b89fbc6ca53e4
Ivan <iponomarev@mail.ru>
2017-05-31 19:41:24 +0000

    a bit of refactoring

--- celesta/src/main/java/ru/curs/celesta/event/TriggerDispatcher.java
+++ celesta/src/main/java/ru/curs/celesta/event/TriggerDispatcher.java
@@ -1,52 +1,46 @@
 package ru.curs.celesta.event;
 
-import org.python.core.PyFunction;
-import ru.curs.celesta.CelestaException;
-import ru.curs.celesta.score.Table;
-import ru.curs.celesta.syscursors.SysCursor;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import java.util.*;
+import org.python.core.PyFunction;
+
+import ru.curs.celesta.CelestaException;
+import ru.curs.celesta.syscursors.SysCursor;
 
 /**
  * Created by ioann on 31.05.2017.
  */
 public class TriggerDispatcher {
 
-  private EnumMap<TriggerType, Map<String, List<PyFunction>>> triggerMap = new EnumMap<>(TriggerType.class);
+	private final EnumMap<TriggerType, Map<String, List<PyFunction>>> triggerMap = new EnumMap<>(TriggerType.class);
 
+	public TriggerDispatcher() {
+		Arrays.stream(TriggerType.values()).forEach(t -> triggerMap.put(t, new HashMap<>()));
+	}
 
-  public TriggerDispatcher() {
-    Arrays.stream(TriggerType.values()).forEach(
-        t -> triggerMap.put(t, new HashMap<>())
-    );
-  }
+	public void registerTrigger(TriggerType type, String tableName, PyFunction pyFunction) {
+		Map<String, List<PyFunction>> tableMap = triggerMap.get(type);
+		tableMap.computeIfAbsent(tableName, s -> new ArrayList<>()).add(pyFunction);
+	}
 
-
-  public void registerTrigger(TriggerType type, String tableName, PyFunction pyFunction) {
-    Map<String, List<PyFunction>> tableMap = triggerMap.get(type);
-
-    if (!tableMap.containsKey(tableName)) {
-      tableMap.put(tableName, new ArrayList<>());
-    }
-
-    tableMap.get(tableName).add(pyFunction);
-  }
-
-
-  public void fireTrigger(TriggerType type, SysCursor cursor) {
-    Map<String, List<PyFunction>> tableMap = triggerMap.get(type);
-
-    try {
-      String tableName = cursor.meta().getName();
-
-      if (tableMap.containsKey(tableName)) {
-        Object[] args = {cursor};
-        tableMap.get(tableName).forEach(f -> f._jcall(args));
-      }
-    } catch (CelestaException e) {
-      throw new RuntimeException(e);
-    }
-
-  }
+	public void fireTrigger(TriggerType type, SysCursor cursor) {
+		Map<String, List<PyFunction>> tableMap = triggerMap.get(type);
+		String tableName;
+		try {
+			tableName = cursor.meta().getName();
+		} catch (CelestaException e) {
+			throw new RuntimeException(e);
+		}
+		List<PyFunction> handlers = tableMap.get(tableName);
+		if (handlers != null) {
+			Object[] args = { cursor };
+			handlers.forEach(f -> f._jcall(args));
+		}
+	}
 
 }
