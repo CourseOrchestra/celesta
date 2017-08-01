3b76e0b7378fa39b6ddd1893290e61e68dbd0961
Boris <toborto@yandex.ru>
2017-08-01 19:12:49 +0000

    Исправление ошибки выгрузки в Эксель из лира-грида (fix #1343)

--- celesta/src/main/java/ru/curs/lyra/LyraFieldValue.java
+++ celesta/src/main/java/ru/curs/lyra/LyraFieldValue.java
@@ -1,15 +1,11 @@
 package ru.curs.lyra;
 
-import static ru.curs.lyra.LyraFormField.DEFAULT_SCALE;
-import static ru.curs.lyra.LyraFormField.REQUIRED;
-import static ru.curs.lyra.LyraFormField.SCALE;
-import static ru.curs.lyra.LyraFormField.SUBTYPE;
+import static ru.curs.lyra.LyraFormField.*;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamWriter;
+import javax.xml.stream.*;
 
 import ru.curs.celesta.CelestaException;
 
@@ -18,18 +14,13 @@ import ru.curs.celesta.CelestaException;
  */
 public final class LyraFieldValue extends LyraNamedElement {
 	static final String XML_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
-	private final LyraFieldType lyraFieldType;
+
 	private final Object val;
-	private final int scale;
-	private final boolean required;
-	private final String subtype;
+	private final LyraFormField lff;
 
 	LyraFieldValue(LyraFormField lff, Object val) throws CelestaException {
 		super(lff.getName());
-		this.lyraFieldType = lff.getType();
-		this.subtype = lff.getSubtype();
-		this.scale = lff.getScale();
-		this.required = lff.isRequired();
+		this.lff = lff;
 		this.val = val;
 	}
 
@@ -43,16 +34,16 @@ public final class LyraFieldValue extends LyraNamedElement {
 	 */
 	public void serialize(XMLStreamWriter xmlWriter) throws XMLStreamException {
 		xmlWriter.writeStartElement(getName());
-		xmlWriter.writeAttribute("type", lyraFieldType.toString());
+		xmlWriter.writeAttribute("type", lff.getType().toString());
 
-		if (subtype != null)
-			xmlWriter.writeAttribute(SUBTYPE, subtype);
+		if (lff.getSubtype() != null)
+			xmlWriter.writeAttribute(SUBTYPE, lff.getSubtype());
 
 		if (val == null)
 			xmlWriter.writeAttribute("null", Boolean.toString(true));
-		if (scale != DEFAULT_SCALE)
-			xmlWriter.writeAttribute(SCALE, Integer.toString(scale));
-		if (required)
+		if (lff.getScale() != DEFAULT_SCALE)
+			xmlWriter.writeAttribute(SCALE, Integer.toString(lff.getScale()));
+		if (lff.isRequired())
 			xmlWriter.writeAttribute(REQUIRED, Boolean.toString(true));
 
 		if (val instanceof Date) {
@@ -72,31 +63,10 @@ public final class LyraFieldValue extends LyraNamedElement {
 	}
 
 	/**
-	 * Тип поля.
+	 * Метаданные поля.
 	 */
-	public LyraFieldType getFieldType() {
-		return lyraFieldType;
-	}
-
-	/**
-	 * Число знаков после запятой.
-	 */
-	public int getScale() {
-		return scale;
-	}
-
-	/**
-	 * Является ли поле обязательным.
-	 */
-	public boolean isRequired() {
-		return required;
-	}
-
-	/**
-	 * Field's subtype.
-	 */
-	public String getSubtype() {
-		return subtype;
+	public LyraFormField meta() {
+		return lff;
 	}
 
 }
