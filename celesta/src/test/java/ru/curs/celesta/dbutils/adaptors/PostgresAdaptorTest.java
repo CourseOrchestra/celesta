e5274c6f5d49e38b8888e850519445601b4e4309
i.golovko <ioanngolovko@gmail.com>
2017-09-26 12:38:05 +0000

    testcontainers for postgres

--- celesta/src/test/java/ru/curs/celesta/dbutils/adaptors/PostgresAdaptorTest.java
+++ celesta/src/test/java/ru/curs/celesta/dbutils/adaptors/PostgresAdaptorTest.java
@@ -1,13 +1,15 @@
 package ru.curs.celesta.dbutils.adaptors;
 
-import java.io.InputStream;
 import java.util.Properties;
 
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.testcontainers.containers.PostgreSQLContainer;
 import ru.curs.celesta.*;
 import ru.curs.celesta.score.Score;
 
@@ -18,31 +20,34 @@ import ru.curs.celesta.score.Score;
 })
 public class PostgresAdaptorTest extends AbstractAdaptorTest {
 
-	public PostgresAdaptorTest() throws Exception {
+	@ClassRule
+	static PostgreSQLContainer postgres = new PostgreSQLContainer();
+
+	private static PostgresAdaptor dba;
+
+	@BeforeClass
+	public static void beforeAll() throws CelestaException {
+		postgres.start();
+
 		Properties params = new Properties();
-		InputStream is = InitTest.class
-				.getResourceAsStream("celesta.postgres.properties");
-		params.load(is);
+		params.put("score.path", "score");
+		params.put("rdbms.connection.url", postgres.getJdbcUrl());
+		params.put("rdbms.connection.username", postgres.getUsername());
+		params.put("rdbms.connection.password", postgres.getPassword());
 
-		AppSettings appSettings = new AppSettings(params);
+		Celesta.initialize(params);
 
-		ConnectionPoolConfiguration cpc = new ConnectionPoolConfiguration();
-		cpc.setJdbcConnectionUrl(appSettings.getDatabaseConnection());
-		cpc.setDriverClassName(appSettings.getDbClassName());
-		cpc.setLogin(appSettings.getDBLogin());
-		cpc.setPassword(appSettings.getDBPassword());
-
-		ConnectionPool.init(cpc);
-
-		DBAdaptor dba = new PostgresAdaptor();
+		dba = new PostgresAdaptor();
 		initMocks(dba);
+	}
 
+	public PostgresAdaptorTest() throws Exception {
 		setDba(dba);
 		setScore(new Score(SCORE_NAME));
 	}
 
 
-	public void initMocks(DBAdaptor dba) throws CelestaException {
+	public static void initMocks(DBAdaptor dba) throws CelestaException {
 		PowerMockito.stub(
 				PowerMockito.method(
 						DBAdaptor.class, "getAdaptor"
