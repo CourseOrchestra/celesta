fa14d15977db4d61da4ed0069d200363b8b0e291
Ivan <iponomarev@mail.ru>
2017-11-01 15:40:37 +0000

    numero sign

--- celesta/celesta-core/src/main/java/ru/curs/lyra/grid/VarcharFieldEnumerator.java
+++ celesta/celesta-core/src/main/java/ru/curs/lyra/grid/VarcharFieldEnumerator.java
@@ -27,7 +27,7 @@ public class VarcharFieldEnumerator extends KeyEnumerator {
 			"<", "=", ">", "«", "»", "§", "©", "¬", "®", "°", "†", "‡", "•", "‰",
 			"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
 			"a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G",
-			"h", "H", "i", "I", "j", "J", "k", "K", "l", "L", "m", "M", "n", "N", "o", "O", "p", "P",
+			"h", "H", "i", "I", "j", "J", "k", "K", "l", "L", "m", "M", "n", "N", "№", "o", "O", "p", "P",
 			"q", "Q", "r", "R", "s", "S", "t", "T", "™", "u", "U", "v", "V", "w", "W",
 			"x", "X", "y", "Y", "z", "Z",
 			"а", "А", "б", "Б", "в", "В", "г", "Г", "д", "Д", "е", "Е", "ё", "Ё",
@@ -259,9 +259,9 @@ public class VarcharFieldEnumerator extends KeyEnumerator {
 
 				ruleBuilder.append("'" + right + "'");
 			}
-
+			System.out.println(ruleBuilder.toString());
 			return ruleBuilder.toString();
-		} catch (Exception e) {
+		} catch (CelestaException e) {
 			throw new RuntimeException(e);
 		}
 	}
