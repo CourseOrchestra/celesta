c8d50b6a2831e24d8d6bff58e17802f2ec576bf8
inponomarev <iponomarev@mail.ru>
2018-08-09 13:11:28 +0300

    further cleanup + making codacy happy

--- celesta-core/src/main/java/ru/curs/celesta/SessionContext.java
+++ celesta-core/src/main/java/ru/curs/celesta/SessionContext.java
@@ -1,11 +1,16 @@
 package ru.curs.celesta;
 
-import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public abstract class SessionContext {
-
+    /**
+     * User id for system tasks, like DB upgrading. Generated randomly each time to prevent its usage by
+     * business logic developers.
+     */
     public static final String SYSTEM_USER_ID = String.format("SYS%08X", ThreadLocalRandom.current().nextInt());
+    /**
+     * Session id for system tasks.
+     */
     public static final String SYSTEM_SESSION_ID = "CELESTA";
 
     private final String userId;
